9c5e2947572d7b47f9b56c0ff340c7673f9f32fe
Rustem Zhumagulov <r.zhumagulov@gmail.com>
2020-08-21 15:31:34 +0600

    oop intro demo

---
+++ src/oop/User.kt
@@ -0,0 +1,59 @@
+package oop
+
+class User {
+    var age: Int = 0
+    set(value) {
+        if (value > 0) field = value
+    }
+
+
+    var name: String? = null
+    get() {
+        return if (field == null) {
+            "No name"
+        } else {
+            field
+        }
+    }
+
+}
+
+//fun main() {
+//    val u = User()
+//    u.age = -1
+//    u.name = null
+//
+//    println("User name ${u.name} and age ${u.age}")
+//}
+
+
+class Pet {
+    var name: String? = null
+    get() {
+       return field?.capitalize()
+    }
+
+    var age: Int = 0
+    set(value) {
+        if(value > 0) field = value
+    }
+
+    var weight: Int = 0
+    set(value) {
+        if(value > 0) field = value
+    }
+
+
+
+
+}
+
+
+fun main() {
+    var p = Pet()
+    p.name = "chizh"
+    p.age = -1
+    p.weight = -20
+
+    println("New pet ${p.name} has ${p.weight} kilos and age ${p.age}")
+}
