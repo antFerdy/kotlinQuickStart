cba5b6edd589e39f7a5227c02196424d81030f9c
Rustem Zhumagulov <r.zhumagulov@gmail.com>
2020-08-24 12:52:25 +0600

    Add filter and map demo

--- src/FunctionalProgrammingBasics.kt
+++ src/FunctionalProgrammingBasics.kt
@@ -1,4 +1,18 @@
+import kotlin.random.Random
+
 fun main() {
+//    lambdaDemo();
+
+//    filterDemo()
+
+//    mapDemo();
+
+    sortDemo()
+}
+
+
+
+fun lambdaDemo() {
     val sum = {a: Int, b: Int -> a + b}
 
     val sum1: (Int, Int) -> Int = {a: Int, b: Int -> a + b}
@@ -15,5 +29,71 @@ fun main() {
     println(squareShortVersion(2))
     println(unitDemo())
 }
+
+
+fun filterDemo() {
+    var numbers = mutableListOf<Int>()
+    for(i in 0..99) {
+        numbers.add(i)
+    }
+
+    var predicate: (Int) -> Boolean = {
+        it % 2 == 0
+    }
+
+    val filteredList = numbers.filter(predicate).toList()
+
+    println("Even numbers")
+    for (n in filteredList) {
+        println(n)
+    }
+
+
+    val oddNumbers = numbers.filter { it % 2 == 1 }.toList()
+    println("Odd numbers")
+    for (odd in oddNumbers) {
+        println(odd)
+    }
+
+
+    val fiveBasedNumbers = numbers.filter{ number: Int -> number % 5 == 0}.toList()
+    println("Divisible to 5")
+    for (number in fiveBasedNumbers) {
+        println(number)
+    }
+
+}
+
+
+fun mapDemo() {
+    //создадим лист через range
+    val numbers = (0..99).toList()
+    val doubledNums = numbers.map { (it * 2).toString() }
+
+    println("Doubled numbers ")
+    for (s in doubledNums) {
+        println(s)
+    }
+
+}
+
+
+fun sortDemo() {
+    var randomArr = mutableListOf<Int>()
+
+    var r = Random(2)
+    for(i in (0..99)) {
+        randomArr.add(r.nextInt())
+    }
+
+    val randomStrNumbers = randomArr.filter { it % 3 == 0 }.map { it * 2 }.sortedDescending().map { "Random transformed number $it" }
+    for (s in randomStrNumbers) {
+        println(s)
+    }
+}
+
+
+
+
 
 
